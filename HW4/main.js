648871948acb4ab62ad984f671c920b473e924e1
EKH0100 <122951863+EKH0100@users.noreply.github.com>
2023-01-28 23:50:45 -0800

    Add files via upload
    As you can see I have commented out some part... writing and rewriting functions over functions... but our whole basic structure of the HTML has been done, JS is just... probably need some help from you. I hope this helps reduces your load cuz it seems like you're deep in bunch of homework this weekend ;)

---
+++ HW4/main.js
@@ -0,0 +1,113 @@
+let selectedExercise;
+let logs = [];
+
+function setCalorieValue(value) {
+  selectedExercise = value;
+}
+
+function validateMinutes(minutes) {
+  if (isNaN(minutes) || minutes < 0) {
+    document.getElementById("minutes").value = "";
+  }
+}
+
+function addCalories() {
+  let minutes = document.getElementById("minutes").value;
+  if (!selectedExercise || isNaN(minutes) || minutes < 0) {
+    document.getElementById("result").value = "Please select an exercise and enter a valid number of minutes.";
+    return;
+  }
+  let calories = minutes * selectedExercise;
+  let log = `${minutes} minutes of ${selectedExercise} calories burnt.`;
+  logs.push(log);
+  document.getElementById("result").value = log;
+  document.getElementById("minutes").value = "";
+  document.getElementById("logs").value = logs.join("\n");
+}
+
+// function mostBurnt() {
+//   if (!logs.length) {
+//     document.getElementById("result").value = "No logs found.";
+//     return;
+//   }
+//   let mostBurnt = 0;
+//   let mostBurntExercise;
+//   logs.forEach(log => {
+//     let calories = parseInt(log.split(" ")[0]) * parseInt(log.split(" ")[4]);
+//     if (calories > mostBurnt) {
+//       mostBurnt = calories;
+//       mostBurntExercise = log.split(" ")[4];
+//     }
+//   });
+//   document.getElementById("result").value = `${mostBurnt} calories burnt in ${mostBurntExercise}.`;
+// }
+
+// function mostBurnt() {
+//   let sitUpsTotal = logs.filter(log => log.includes("Sit Ups")).reduce((acc, log) => acc + log.split(" ")[0], 0);
+//   let pushUpsTotal = logs.filter(log => log.includes("Push Ups")).reduce((acc, log) => acc + log.split(" ")[0], 0);
+//   let jumpRopeTotal = logs.filter(log => log.includes("Jump Rope")).reduce((acc, log) => acc + log.split(" ")[0], 0);
+
+//   let maxCaloriesBurned = Math.max(sitUpsTotal, pushUpsTotal, jumpRopeTotal);
+//   let exercise = "";
+
+//   if (maxCaloriesBurned === sitUpsTotal) {
+//     exercise = "Sit Ups";
+//   } else if (maxCaloriesBurned === pushUpsTotal) {
+//     exercise = "Push Ups";
+//   } else {
+//     exercise = "Jump Rope";
+//   }
+
+//   document.getElementById("result").value = `Most calories burnt by ${exercise} with ${maxCaloriesBurned} calories`;
+// }
+function mostBurnt() {
+  let sitUpsTotal = 0;
+  let pushUpsTotal = 0;
+  let jumpRopeTotal = 0;
+
+  logs.forEach(log => {
+    let logData = log.split(" ");
+    let exercise = logData[1];
+    let minutes = logData[0];
+    let calories = 0;
+
+    if (exercise === "Sit Ups") {
+      calories = 10 * minutes;
+      sitUpsTotal += calories;
+    } else if (exercise === "Push Ups") {
+      calories = 15 * minutes;
+      pushUpsTotal += calories;
+    } else if (exercise === "Jump Rope") {
+      calories = 18 * minutes;
+      jumpRopeTotal += calories;
+    }
+  });
+
+  let maxCaloriesBurned = Math.max(sitUpsTotal, pushUpsTotal, jumpRopeTotal);
+  let exercise = "";
+
+  if (maxCaloriesBurned === sitUpsTotal) {
+    exercise = "Sit Ups";
+  } else if (maxCaloriesBurned === pushUpsTotal) {
+    exercise = "Push Ups";
+  } else {
+    exercise = "Jump Rope";
+  }
+
+  document.getElementById("result").value = `${exercise} ${maxCaloriesBurned} calories burnt.`;
+}
+
+
+function showAll() {
+  if (!logs.length) {
+    document.getElementById("result").value = "No logs found.";
+    return;
+  }
+  let logsList = document.getElementById("logs-list");
+  logsList.innerHTML = "";
+  logs.forEach(log => {
+    let li = document.createElement("li");
+    li.innerText = log;
+    logsList.appendChild(li);
+  });
+}
